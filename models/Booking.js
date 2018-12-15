1ecb448d9d29168a311cb67e6a4bc74d58152dd5
katerina patticha <kate@kpatticha.com>
2018-12-15 22:54:39 +0100

    Add schema for booking #8

---
+++ models/Booking.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var BookingSchema = new mongoose.Schema({
+  user: { type : Schema.ObjectId, ref : 'User' },
+  uui: Number,
+  message: String,
+  flightNo: String,
+  passengers: Number,
+  oneWay: Boolean,
+  departure: String,
+  arrival: String,
+  departureTimestamp: { type: Date },
+  arrivalTimestamp: { type: Date },
+  returnTimestamp: { type: Date },
+  duration: Number,
+  distance: Number,
+  confirmationToken: String,
+  confirmedAt: { type: Date },
+  confirmationSentAt: { type: Date },
+  createdAt: { type: Date },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Booking', BookingSchema);
