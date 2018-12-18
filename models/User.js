cac744b3ad541a75c43c3c304b7c87060d296591
katerina patticha <kate@kpatticha.com>
2018-12-18 01:30:18 +0100

    Add admin flag in user's model #11

--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 
 var UserSchema = new mongoose.Schema({
   uui: Number,
+  admin: { type: Boolean, default: false },
   firstName: String,
   lastName: String,
   email: String,
