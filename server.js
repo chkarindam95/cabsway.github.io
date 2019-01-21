4bd460d12434371d98aa970976726da41c861fd0
katerina patticha <kate@kpatticha.com>
2019-01-21 22:38:26 +0100

    Add session middleware #33

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import session from 'express-session';
 import bodyParser from 'body-parser';
 import config, {nodeEnv} from './config/env/development';
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const server = express();
 const db = connect();
@@ -10,13 +12,35 @@ db.once('open', () => {
   listen();
 });
 
+const store = new MongoDBStore(
+  {
+    uri: config.db,
+    collection: 'sessions'
+  },
+  function(error) {
+    console.error(error);
+  });
+
 const prices = require('./routes/prices');
 const users = require('./routes/users');
 const bookings = require('./routes/bookings');
 const location = require('./routes/location');
+const auth = require('./routes/auth');
 
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: false }));
+
+server.use(
+  session(
+    {
+      secret: 'my secret',
+      resave: false, // not be saved on any response, only if sth change,
+      saveUninitialized: false,
+      store: store
+    }
+  )
+);
+
 
 server.set('view engine', 'ejs');
 server.use(express.static('dist'));
@@ -25,10 +49,13 @@ server.get('/', (req, res) => {
   res.render('index');
 });
 
+// Routinstaes
+
 server.use('/prices', prices);
 server.use('/users', users);
 server.use('/bookings', bookings);
 server.use('/location', location);
+server.use('/auth', auth);
 
 // Connection
 
